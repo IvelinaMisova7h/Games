0e33b7c369d33e2f4ab820b67dd25db822acbcd9
ivelinamisova <ivelina_misova@abv.bg>
2022-08-12 14:58:51 +0300

    init

--- pong.py
+++ pong.py
@@ -11,15 +11,27 @@ paddle_a = turtle.Turtle()
 paddle_a.speed(0)    # Speed of animation. Sets the speed to the max.
 paddle_a.shape("square")
 paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
 paddle_a.penup()     # No drawing when moving
 paddle_a.goto(-350, 0)
 
 # Paddle B
-
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
 
 
 # Ball
-
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
 
 
 
