74ebee36aa544ce90994e722a2b866e0f2e88cc7
ivelinamisova <ivelina_misova@abv.bg>
2022-08-14 22:51:54 +0300

    init

--- pong.py
+++ pong.py
@@ -6,6 +6,10 @@ window.bgcolor("black")
 window.setup(width=800, height=600)
 window.tracer(False)      # Speed up
 
+# Score
+score_a = 0
+score_b = 0
+
 # Paddle A
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)    # Speed of animation. Sets the speed to the max.
@@ -30,6 +34,15 @@ ball.penup()
 ball.goto(0, 0)
 ball.dx = 1   # User defined object properties
 ball.dy = 1
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0 Player B: 0 ", align="center", font=("Courier", 24, "normal"))
 
 # Function
 
@@ -88,10 +101,16 @@ while True:
     if ball.xcor() > 390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {} Player B: {} ".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
 
     if ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {} Player B: {} ".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
 
     # Paddle and ball collision
     if (340 < ball.xcor() < 350) and (paddle_b.ycor() + 40 > ball.ycor() > paddle_b.ycor() - 40):
