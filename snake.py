927c20fc8b9a9e6d572aee758a1bc4053757c352
ivelinamisova <ivelina_misova@abv.bg>
2022-08-17 12:46:12 +0300

    init

--- snake.py
+++ snake.py
@@ -1,9 +1,32 @@
-import pygame
 import turtle
+
+delay = 0.1
+
+# Score
+score = 0
+high_score = 0
 
 # Creating the screen
 screen = turtle.Screen()
 screen.title("Snake")
 screen.bgcolor("green")
 screen.setup(width=600, height=800)
-screen.tracer(False)
+screen.tracer(0)  # Turns screen updates
+
+# Creating the snake
+snake = turtle.Turtle()
+snake.speed(0)
+snake.shape("square")
+snake.color("purple")
+snake.penup()      # No drawing when moving
+snake.goto(0, 0)
+snake.direction = "stop"
+
+# Snake food
+food = turtle.Turtle()
+food.speed(0)
+food.shape("circle")
+food.color("white")
+food.penup()
+food.goto(0, 100)
+
